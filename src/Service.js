df2854b714ee4b18b4e78ea86f20eab755750d11
Manojkumar Krishnaramanujam Baskaran <58929289+MANOJKUMAR877@users.noreply.github.com>
2022-11-10 09:23:12 +0100

    current

--- src/Service.js
+++ src/Service.js
@@ -16,6 +16,35 @@ function Service() {
 
       <div className="container">
         <div className="row">
+        <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+                  <line x1="8" y1="21" x2="16" y2="21"></line>
+                  <line x1="12" y1="17" x2="12" y2="21"></line>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">
+                  Software Developer - ( HumanAize || Amicocon) - work student
+                </h1>
+                <h1 className="service__text">From (01/06/2022 - Till)</h1>
+               
+                {/* <p className="p service__text p__color">close to the earth.</p> */}
+              </div>
+            </div>
+          </div>
           <div className="col__3">
             <div className="service__box pointer">
               <div className="icon">
